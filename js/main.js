b03b4f580a5f3a1e94026729ef03a2be4e579b6b
BOUTAYNA02 <zakariboutayna02@gmail.com>
2025-01-12 23:03:34 +0100

    main.js

--- js/main.js
+++ js/main.js
@@ -1,56 +1,53 @@
-function LibraryBooks() {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-        .then(response => {
-            return response.json()
-})
-        .then(data => {
-            books = [];
-            bookstoborrow = [];
-            for (let i = 0; i < data.length; i++) {
-                books[books.length] = { title: data[i].title };
-                bookstoborrow[bookstoborrow.length] = false; // Book is available initially
-            }
-        })
-        .catch(error =>
-            alert('The Errors  fetching the books library:'+ error.name));
-}
+fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        let books = [];
+        let bookstoborrow = [];
 
-function viewtheBooks() {
-    let available_Books = [];
-    for (let i = 0; i < books.length; i++) {
-        if (!bookstoborrow[i]) {
-            available_Books[available_Books.length] = books[i];
+        for (let i = 0; i < data.length; i++) {
+            books[books.length] = { title: data[i].title };
+            bookstoborrow[bookstoborrow.length] = false; // Book is available initially
         }
-    }
-    console.log("Available Books:", available_Books);
-}
 
-function borrowBook(book) {
-    if (!bookstoborrow[book]) {
-        bookstoborrow[book] = true;
-        console.log(`Borrowed: ${books[book].title}`);
+        let available_Books = [];
+        for (let i = 0; i < books.length; i++) {
+            if (!bookstoborrow[i]) {
+                available_Books[available_Books.length] = books[i];
+            }
+        }
+        console.log("Available Books:", available_Books);
 
-    }
-}
+        let book = 0;
+        if (!bookstoborrow[book]) {
+            bookstoborrow[book] = true;
+            console.log(`Borrowed: ${books[book].title}`);
+        }
 
-function returnBook(book) {
-    if (bookstoborrow[book]) {
-        bookstoborrow[book] = false;
-        console.log(`Returned: ${books[book].title}`);
-    } else {
-        console.log("Book isn't borrowed.");
-    }
-}
+        available_Books = [];
+        for (let i = 0; i < books.length; i++) {
+            if (!bookstoborrow[i]) {
+                available_Books[available_Books.length] = books[i];
+            }
+        }
+        console.log("Available Books After Borrowing:", available_Books);
 
-function Library() {
-    LibraryBooks();
-    setTimeout(() => {
-        viewtheBooks();
-        borrowBook(0);
-        viewtheBooks();
-        returnBook(0);
-        viewtheBooks();
-    }, 1000); 
-}
+        if (bookstoborrow[book]) {
+            bookstoborrow[book] = false;
+            console.log(`Returned: ${books[book].title}`);
+        } else {
+            console.log("Book isn't borrowed.");
+        }
 
-Library();
+        available_Books = [];
+        for (let i = 0; i < books.length; i++) {
+            if (!bookstoborrow[i]) {
+                available_Books[available_Books.length] = books[i];
+            }
+        }
+        console.log("Available Books After Returning:", available_Books);
+    })
+    .catch(error => {
+        alert('The Errors fetching the books library:' + error.name);
+    });
