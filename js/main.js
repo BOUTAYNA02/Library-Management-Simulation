689915dd40e74f34cfb1dce1ac041451d3506738
BOUTAYNA02 <zakariboutayna02@gmail.com>
2025-01-12 22:56:47 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,56 @@
+function LibraryBooks() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+        .then(response => {
+            return response.json()
+})
+        .then(data => {
+            books = [];
+            bookstoborrow = [];
+            for (let i = 0; i < data.length; i++) {
+                books[books.length] = { title: data[i].title };
+                bookstoborrow[bookstoborrow.length] = false; // Book is available initially
+            }
+        })
+        .catch(error =>
+            alert('The Errors  fetching the books library:'+ error.name));
+}
+
+function viewtheBooks() {
+    let available_Books = [];
+    for (let i = 0; i < books.length; i++) {
+        if (!bookstoborrow[i]) {
+            available_Books[available_Books.length] = books[i];
+        }
+    }
+    console.log("Available Books:", available_Books);
+}
+
+function borrowBook(book) {
+    if (!bookstoborrow[book]) {
+        bookstoborrow[book] = true;
+        console.log(`Borrowed: ${books[book].title}`);
+
+    }
+}
+
+function returnBook(book) {
+    if (bookstoborrow[book]) {
+        bookstoborrow[book] = false;
+        console.log(`Returned: ${books[book].title}`);
+    } else {
+        console.log("Book isn't borrowed.");
+    }
+}
+
+function Library() {
+    LibraryBooks();
+    setTimeout(() => {
+        viewtheBooks();
+        borrowBook(0);
+        viewtheBooks();
+        returnBook(0);
+        viewtheBooks();
+    }, 1000); // Delay to ensure books are fetched before interacting with them
+}
+
+Library();
